4e82b41cd90e00af285b67662aa0aed92cca7341
tbronny <tristan.bron7@gmail.com>
2021-08-23 11:07:49 -0500

    initial commit - place names translated correctly

--- place-names/js/placeNames/main.js
+++ place-names/js/placeNames/main.js
@@ -1,12 +1,26 @@
 // Put your code here
 
 const names = [
-   "Nashville", "Hong Kong", "The back yard", "Earth",
-    "London", "The mall", "Ryman Auditorium", "The Great Wall of China"
-];
+    "Nashville",
+    "Hong Kong",
+    "The back yard",
+    "Earth",
+    "London",
+    "The mall",
+    "Ryman Auditorium",
+    "The Great Wall of China",
+]
 
-console.log("All Pace Names");
+console.log("All Pace Names")
 
 for (const name of names) {
-  console.log(name);
+    console.log(name)
+}
+
+const theNames = names.filter((n) => n.startsWith("The"))
+
+console.log(`${theNames}`)
+
+for (const name of theNames) {
+    console.log(name)
 }
