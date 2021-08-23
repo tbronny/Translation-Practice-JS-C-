543e540fc16512d900611e31c2f1f2e4cce198c3
tbronny <tristan.bron7@gmail.com>
2021-08-23 15:43:19 -0500

    translated to js; ints still need to be converted to string

--- rolling-dice/js/rollingDice/main.js
+++ rolling-dice/js/rollingDice/main.js
@@ -1,1 +1,23 @@
 // Put your code here
+
+console.log("Let's roll some dice, baby!")
+console.log("---------------------------")
+
+const Roll = (min, max) => {
+    min = Math.ceil(1)
+    max = Math.floor(6)
+
+    return Math.floor(Math.random() * (max - min + 1)) + min
+}
+
+for (let i = 0; i < 10; i++) {
+    const die1 = Roll()
+    const die2 = Roll()
+
+    let message = `${die1} + ${die2} == ${die1 + die2}`
+    if (die1 === die2) {
+        message += " DOUBLES!"
+    }
+
+    console.log(message)
+}
