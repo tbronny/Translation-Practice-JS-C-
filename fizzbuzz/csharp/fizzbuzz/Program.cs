ce608397c59ca7b3ec8303d2b8227b60a68c6218
tbronny <tristan.bron7@gmail.com>
2021-08-27 13:28:25 -0500

    fizzbuzz translated to csharp

--- fizzbuzz/csharp/fizzbuzz/Program.cs
+++ fizzbuzz/csharp/fizzbuzz/Program.cs
@@ -6,7 +6,31 @@ namespace fizzbuzz
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            for (int n = 1; n <= 100; n++)
+            {
+                if (n % 15 == 0)
+                {
+                    Console.WriteLine("fizzbuzz");
+                }
+                else
+                {
+                    if (n % 3 == 0)
+                    {
+                        Console.WriteLine("fizz");
+                    }
+                    else
+                    {
+                        if (n % 5 == 0)
+                        {
+                            Console.WriteLine("buzz");
+                        }
+                        else
+                        {
+                            Console.WriteLine(n);
+                        }
+                    }
+                } 
+            }
         }
     }
 }
